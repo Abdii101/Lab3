ed7f3d5c143440b936cda2940d62705188f3e137
Abdirahman Aden <83236236+Abdii101@users.noreply.github.com>
2021-06-10 17:43:28 +0000

    1

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-lists:
-a)
 
-n = input("insert number ;") # type down you numbers and press space after each number
-a = n.split() 
-print(a)
+#lists:
+#a)
 
-b)
+#n = input("insert number ;") # type down you numbers and press space after each number
+#a = n.split() 
+#print(a)
 
-n =
+#b)
+
+#a = input("insert a list").split()
+#a = [1,2,3,4,5]
+#print(a[1])
+#print(a[3])
+
+#funuction:
+#a)
+
+#def sum(x,y):
+  #return x+y
+#a=int(input("Enter the first number ;"))
+#b=int(input("Enter the second number ;"))
+#c=sum(a,b)
+#print("sum of",a,"and",b, "is",c)
+
+#b)
+def num_circles():
+a= input()
